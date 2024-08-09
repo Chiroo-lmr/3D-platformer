35ca42993f648fd0ca4d19c7ad27e1e7c6b67164
Chirootao <126762943+Chiroo-lmr@users.noreply.github.com>
2024-08-09 17:19:31 +0200

    finished level with coin

--- scenes/character/player/player.gd
+++ scenes/character/player/player.gd
@@ -4,7 +4,8 @@ var xform : Transform3D
 var speed = 1
 const WALK_SPEED = 5.0
 const SPRINT_SPEED = 8.0
-const JUMP_VELOCITY = 4
+const SNEAK_SPEED = 3
+const JUMP_VELOCITY = 4.5
 const SENSITIVITY = 0.005
 
 const CHARACTER_FREQ = 2.0
@@ -12,10 +13,8 @@ const CHARACTER_AMP = 0.08
 var t_CHARACTER = 0.0 
 
 const BASE_FOV = 75.0
-const FOV_CHANGE = 1.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 9.8
+const FOV_CHANGE = 0.1
+var gravity = 13
 @onready var neck = $Neck
 @onready var camera = $Neck/POV
 
@@ -31,31 +30,23 @@ func _unhandled_input(event):
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(90))
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle jump.
 	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
+		velocity.y = JUMP_VELOCITY 
 	if Input.is_action_pressed("sprint"):
 		speed = SPRINT_SPEED
 	else:
 		speed = WALK_SPEED
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	#avoir la caméra qui bouge ( jsp si je le met)
+	#if is_on_floor() and input_dir != Vector2(0, 0) or is_on_floor_only():
+	#	align_with_floor($RayCast3D.get_collision_normal())
+	#	global_transform = global_transform.interpolate_with(xform, 0.3)
+	#elif not is_on_floor():
+	#	align_with_floor(Vector3.UP )
+	#	global_transform = global_transform.interpolate_with(xform, 0.3)
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	if is_on_floor() and input_dir != Vector2(0, 0) or is_on_floor_only():
-		align_with_floor($RayCast3D.get_collision_normal())
-		global_transform = global_transform.interpolate_with(xform, 0.3)
-	elif not is_on_floor():
-		align_with_floor(Vector3.UP )
-		global_transform = global_transform.interpolate_with(xform, 0.3)
-	
 	if is_on_floor():
 		if direction:
 			velocity.x = direction.x * speed
@@ -69,13 +60,10 @@ func _physics_process(delta):
 		
 	t_CHARACTER += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _head_CHARACTER(t_CHARACTER)
-	
 	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
 	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
 	if is_on_floor():
-		
 		camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
-	
 	move_and_slide()
 
 func _head_CHARACTER(time) -> Vector3:
--- scenes/levels/world.gd
+++ scenes/levels/world.gd
@@ -13,5 +13,15 @@ func _process(delta):
 func _on_coin_body_entered(body):
 	if body == player:
 		Global.coins_collected += 1
-		collectable.position.x = randi_range(-25, 25)
-		collectable.position.z = randi_range(-25, 25)
+		if Global.coins_collected == 1:
+			collectable.position = Vector3(-1, 13.5, -21)
+		elif Global.coins_collected == 2:
+			collectable.position = Vector3(-21, 10, -13)
+		elif Global.coins_collected == 3:
+			collectable.position = Vector3(1, 12.5, 1)
+		elif Global.coins_collected == 4:
+			collectable.position = Vector3(-7, 16.5, 11)
+		elif Global.coins_collected == 5:
+			collectable.position = Vector3(-1, 1, -21)
+		elif Global.coins_collected == 6:
+			collectable.position = Vector3(0, 1, 0)
