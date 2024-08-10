e895aa9ecd23d1eacc48c0053b3c4112e960303e
Chirootao <126762943+Chiroo-lmr@users.noreply.github.com>
2024-08-10 22:56:00 +0200

    last update i think (gonna make another game)

--- scenes/levels/world.gd
+++ scenes/levels/world.gd
@@ -3,10 +3,10 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
 @onready var collectable = $Coin
 @onready var player = $Player
 
--- scenes/menu/hud.gd
+++ scenes/menu/hud.gd
@@ -11,3 +11,21 @@ func _process(delta):
 
 	if Global.coins_collected:
 		coins.text = "Coins récupérés : " + str(Global.coins_collected)
+		if Global.coins_collected != 7:
+			time = float(time) + delta
+			update_ui()
+
+
+var time = Global.speedrun_time
+	
+func update_ui():
+	# Format time with two decimal places
+	var formatted_time = str(time)
+	var decimal_index = formatted_time.find(".")
+	
+	if decimal_index > 0:
+		formatted_time = formatted_time.left(decimal_index + 3)  # Take only two decimal places
+	
+	Global.speedrun_time = formatted_time
+		
+	$VBoxContainer2/Label.text = formatted_time
